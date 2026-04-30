15649d8671050a708282f01f52a55d2c26e9242a
ibnukece <ibnuuuka@student.uns.ac.id>
2026-04-30 13:52:13 +0700

    fxsj

--- api/logout.php
+++ api/logout.php
@@ -6,6 +6,6 @@ session_start();
 session_unset();
 session_destroy();
 
-header("Location: ../index.php");  // ✅ tetap ../ karena index.php ada di root
+header("Location: index.php");  // ✅ tetap ../ karena index.php ada di root
 exit;
 ?>
